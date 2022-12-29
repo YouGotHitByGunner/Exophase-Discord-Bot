3c43de9f0b9181c5c0b0539e217e3ab2d5cf4b98
Pal Costin <costin.pal98@gmail.com>
2022-12-29 03:56:07 +0200

    Added a Last Played feature!

--- commands/profile.js
+++ commands/profile.js
@@ -120,6 +120,7 @@ module.exports = {
 					{ name: '<:trophy:1053286071343001630> Awards Earned:', value: `${exophase.earned_awards.toLocaleString("en-US")}`, inline: true},
 					{ name: '<:trophyunearned:1055617148430598245> Unearned Awards:', value: `${unearnedAchievements.toLocaleString("en-US")}`, inline: true},
 					{ name: 'Last Activity:', value: `[${exophase3.game.title}](${exophase3.game.endpoint_awards}) (${exophase3.game.platforms[0].name})\n${progressBar}\n${exophase2.games[0].earned_awards} of ${exophase2.games[0].total_awards} (${percentageLastGame.toFixed()}%) achievements earned.` },
+					{ name: 'Last Played:', value: `<t:${exophase2.games[0].lastplayed}:D>`}
 				);
 
 				let commandMessage;
